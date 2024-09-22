4d191a74582c30fff817ae61ac57491f3ba2933e
dev-rfgul <rfgul587@gmail.com>
2024-09-22 16:22:56 +0500

    completed the video, user creation,user auth, sign up login functionality learned in this video

--- app.js
+++ app.js
@@ -33,12 +33,41 @@ app.post('/create', async (req, res) => {
     });
 
     let token = jwt.sign({ email }, "heloworld")
-    res.cookie('token',token);
+    res.cookie('token', token);
     res.send(createdUser);
 
 
 
 });
+
+app.get('/login', (req, res) => {
+    res.render('login')
+})
+app.post('/login', async (req, res) => {
+    let user = await userModel.findOne({ email: req.body.email })
+    console.log(user)
+    if (!user) res.send("Email or Password is Incorrect")
+    bcrypt.compare(req.body.password, user.password, (err, result) => {
+
+        console.log(result)
+        // it will return true if the password is correct
+
+        if (result) {
+            let token =jwt.sign({email: user.email},'heloworld');
+            res.cookie('token',token)
+            res.send("Logged In");
+        }
+        else res.send("Email or Password is Incorrect")
+    })
+
+})
+
+
+// to logout we just need to remove the cookie 
+app.get('/logout', (req, res) => {
+    res.cookie("token", ""),
+        res.redirect('/')
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
