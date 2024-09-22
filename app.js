b6ab30cdc99a1aae841cae304525b280faff606e
dev-rfgul <rfgul587@gmail.com>
2024-09-22 15:44:01 +0500

    create user task completed

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
-
 const path = require('path');
 const cookieParser = require('cookie-parser');
-
-const userModel=require('./models/user')
+const bcrypt = require('bcrypt');
+const userModel = require('./models/user');
+const jwt = require('jsonwebtoken');
 
 app.set('view engine', 'ejs');
 app.use(express.json());
@@ -15,6 +15,31 @@ app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.render('index');
-})
+});
 
-app.listen(3000)
+app.post('/create', async (req, res) => {
+    let { username, email, password, confirmPassword } = req.body;
+
+
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    let createdUser = await userModel.create({
+        username,
+        email,
+        password: hash,
+        // confirmPassword: confirmPassword // Usually not stored
+    });
+
+    let token = jwt.sign({ email }, "heloworld")
+    res.cookie('token',token);
+    res.send(createdUser);
+
+
+
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const mongoose=require('mongoose');
-
-mongoose.connect(`mongodb+srv://fahad:WXX6mMBb8q6qS6E@cluster0.vz7c8.mongodb.net/`);
+mongoose.connect('mongodb+srv://fahad:WXX6mMBb8q6qS6E@cluster0.vz7c8.mongodb.net/myDatabase?retryWrites=true&w=majority');
 
 const userSchema=new mongoose.Schema({
     username:String,
